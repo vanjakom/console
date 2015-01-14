0ac9250fce70bf51a6bfe301c79a229d7a90d893
Vanja Komadinovic <vanjakom@gmail.com>
2015-01-15 00:25:50 +0100

    merging work

--- src/console/core.clj
+++ src/console/core.clj
@@ -1,6 +1,25 @@
 (ns console.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(use 'clojure.java.io)
+
+(defn load-class-loader [jar-path]
+	(def cl (java.net.URLClassLoader/newInstance 
+			(into-array java.net.URL [(new java.net.URL jar-path)]))))
+
+(defn load-class [class-loader class-name]
+	(.newInstance (.loadClass class-loader class-name)))
+
+(defn jar-path-in-maven [group artifact version] 
+	(str "~/.m2/repository/" (clojure.string/replace group #"\." "/") "/" 
+		artifact "/" version "/" artifact "-" version ".jar"))
+
+(defn set-engine engine-class)
+
+(defn run-task [mapper-fn reducer-fn input-path output-path] (println "running task"))
+
+(defn read-file [file-path state func] 
+	(with-open [rdr (reader "/tmp/test.txt")]
+  		(doseq [line (line-seq rdr)]
+    		(println line))))
+
+
